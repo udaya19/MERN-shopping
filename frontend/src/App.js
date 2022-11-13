ba949d83c06d883d750cacb6576a81d89700c63c
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-11-13 11:44:47 +0530

    Header UI and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/home/Home";
+import Header from "./components/layout/Header";
+import "./styles/app.scss";
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/layout/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { IoFastFoodOutline } from "react-icons/io5";
+import { FiShoppingCart, FiLogIn } from "react-icons/fi";
+import { FaUser } from "react-icons/fa";
+import "../../styles/header.scss";
+import { Link } from "react-router-dom";
+
+const Header = ({ isAuthenticated = false }) => {
+  return (
+    <>
+      <nav>
+        <div>
+          <IoFastFoodOutline />
+        </div>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/about">About</Link>
+          <Link to="/cart">
+            <FiShoppingCart />
+          </Link>
+          <Link to={isAuthenticated ? "/me" : "/login"}>
+            {isAuthenticated ? <FaUser /> : <FiLogIn />}
+          </Link>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
