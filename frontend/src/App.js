c03d80b5e90e6d5d3308b0178cdb4f84a606969e
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-11-15 21:20:15 +0530

    Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
+import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import "./styles/app.scss";
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../styles/footer.scss";
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <h2>Burger Wala</h2>
+        <p>Experience the best burger in Vijayawada</p>
+        <strong>
+          All rights reserved. Developed by Gonuguntla Udaya Kiran
+        </strong>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
